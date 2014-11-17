c2ae0b00490ca8580b901e88e7486bb18ae7b1b6
fashionfz <ibcm@qq.com>
2014-11-17 16:24:52 +0800

    nio

--- src/org/ty/yaxin/XSelector.java
+++ src/org/ty/yaxin/XSelector.java
@@ -1,15 +1,20 @@
 package org.ty.yaxin;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.Set;
 
-public class XSelector {
+import com.sun.xml.internal.messaging.saaj.util.CharReader;
+
+public class XSelector{
 
 	public static void main(String[] args) {
 		try {
@@ -36,23 +41,37 @@ public class XSelector {
 				Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
 				while (keyIterator.hasNext()) {
 					SelectionKey key = keyIterator.next();
-					if (key.isAcceptable()) {
-						System.out.println("siAccept");
-					} else if (key.isConnectable()) {
-						System.out.println("isConnection");
-					} else if (key.isReadable()) {
-						System.out.println("isRead");
-						channel = (SocketChannel) key.channel();
-						buf.clear();
-						channel.read(buf);
-						buf.flip();
-						while(buf.hasRemaining()){
-							System.out.println(buf.get());
-						}
-					} else if (key.isWritable()) {
-						System.out.println("isWrite");
-					}
 					keyIterator.remove();
+					if (key.isAcceptable()) {
+						System.out.println("#siAccept");
+					} else if (key.isConnectable()) {
+						System.out.println("#isConnection");
+					} else if (key.isReadable()) {
+						System.out.println("#isRead");
+						channel = (SocketChannel) key.channel();
+						int len = 0;
+						buf.clear();
+						StringBuffer sb = new StringBuffer();
+						while((len = channel.read(buf))>0){
+							buf.flip();
+							sb.append(new String(buf.array(),0,len));
+						}
+						System.out.println(sb.toString());
+						key.interestOps(SelectionKey.OP_WRITE);
+
+						
+					} else if (key.isWritable()) {
+						System.out.println("#isWrite");
+						
+						Scanner cin=new Scanner(System.in);
+						String input=cin.nextLine();
+						buf.clear();
+						buf.put(input.getBytes());
+						buf.flip();
+						channel.write(buf);
+						key.interestOps(SelectionKey.OP_READ);
+						System.out.println("client write over");
+					}
 				}
 			}
 
--- src/org/ty/yaxin/XServer.java
+++ src/org/ty/yaxin/XServer.java
@@ -3,35 +3,122 @@ package org.ty.yaxin;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
 
-public class XServer {
+public class XServer implements Runnable{
 
 	public static void main(String[] args){
+		new Thread(new XServer()).start();
+	}
+	
+	private ServerSocketChannel ssc;
+	private Selector selector;
+	private SelectionKey skey;
+	
+	public void init(){
 		try {
-			ServerSocketChannel server = ServerSocketChannel.open();
-			server.socket().bind(new InetSocketAddress(9999));
-			while(true){
-				SocketChannel channel = server.accept();
-				ByteBuffer buf = ByteBuffer.allocate(48);
-				int length = channel.read(buf);
-				buf.flip();
-				while(length!=-1){
-					System.out.println("length"+length);
-					while(buf.hasRemaining()){
-						System.out.println("server:"+(char)buf.get());
+			ssc = ServerSocketChannel.open();
+			ssc.socket().bind(new InetSocketAddress(9999));
+			ssc.configureBlocking(false);
+			selector = Selector.open();
+			skey = ssc.register(selector, SelectionKey.OP_ACCEPT);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		init();
+		while(true){
+			try {
+				if(selector.select()>0){
+					Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+					while(it.hasNext()){
+						SelectionKey key = it.next();
+						it.remove();
+						if(!key.isValid()){
+							continue;
+						}
+						else if(key.isAcceptable()){
+							System.out.println("#conn");
+							conn(key);
+							
+						}else if(key.isReadable()){
+							System.out.println("#read");
+							read(key);
+							
+						}else if(key.isWritable()){
+							System.out.println("#write");
+							write(key);
+						}
 					}
-					break;
 				}
-				System.out.println("server read over");
-				buf.clear();
-				buf.put("server answer".getBytes());
-				channel.write(buf);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
+		}
+	}
+	
+	public void conn(SelectionKey key){
+		
+		try {
+			ServerSocketChannel channel = (ServerSocketChannel) key.channel();
+			SocketChannel client = channel.accept();
+			client.configureBlocking(false);
+			client.register(selector, SelectionKey.OP_READ);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void read(SelectionKey key){
+		SocketChannel channel = (SocketChannel) key.channel();
+		ByteBuffer buf = ByteBuffer.allocate(48);
+		buf.clear();
+		StringBuffer sb = new StringBuffer();
+		int len=0;
+		try {
+			while((len = channel.read(buf))>0){
+				buf.flip();
+				sb.append(new String(buf.array(), 0, len));  
+			}
+			if(sb.length()>0){
+				System.out.println(sb.toString());
+			}
+			if("quit".equals(sb.toString().trim())){
+				channel.write(ByteBuffer.wrap("quit".getBytes()));
+				key.cancel();
+				channel.close();
+				channel.socket().close();
+			}else{
+				key.interestOps(SelectionKey.OP_WRITE | key.interestOps());
+			}
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+	}
+	
+	public void write(SelectionKey key){
+		Scanner cin=new Scanner(System.in);
+		String input=cin.nextLine();
+		SocketChannel channel = (SocketChannel) key.channel();
+		try {
+			channel.write(ByteBuffer.wrap(input.getBytes()));
+			key.interestOps(SelectionKey.OP_READ);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
